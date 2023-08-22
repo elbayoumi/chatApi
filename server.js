f9212a0c76fe1e8e7fd449d4bbe6bdb2853320f0
mohamed <mohamedashrafelbayoumi@gmail.com>
2023-08-22 13:15:11 +0300

    v1

--- server.js
+++ server.js
@@ -17,24 +17,24 @@ app.get('/', (req, res) => {
 })
 io.on('connection', (socket) => {
     console.log('connection');
-    socket.on("sendChatToServer", (messege) => {
+    socket.on("sendBus", (messege) => {
         // console.log(messege);
-        io.sockets.emit('sendChatToClient', messege);
-
+        if (checkToken(messege) == "supervisor") {
+            io.sockets.emit('receiveBus', messege);
+        } else if (checkToken(messege) == "driver") {
+            io.sockets.emit('receiveBus', messege);
+        }
         ///to make broadcast
         // socket.broadcast.emit('sendChatToClient', messege)
         // socket.broadcast.emit('sendChatTyping', messege);
-    })
-    socket.on("sendChatToServerTyping", (messege) => {
-        // console.log(messege);
-        ///to make broadcast
-        socket.broadcast.emit('sendChatToClientTyping', messege);
     })
     socket.on('desconnect', (socket) => {
         console.log('Desconnect');
     })
 });
-
+function checkToken(messege) {
+    return JSON.parse(messege).token == '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' ? JSON.parse(messege).type : false
+}
 server.listen(3000, () => {
     console.log("Server listening on port 3000 is runing")
 });
