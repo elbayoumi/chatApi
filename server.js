263fb91605365e5f148d45e4e5e00ee22cc46026
mohamed <mohamedashrafelbayoumi@gmail.com>
2023-08-23 10:24:33 +0300

    v1.q

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const server = require("http").createServer(app);
+const jwt = require('jsonwebtoken');
 const io = require("socket.io")(server, {
     cors: {
         origin: "*"
 
     }
 });
-const JWT_SECRET_KEY = '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';    
+const JWT_SECRET_KEY = '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';
 // const socket = require("socket.io-client")("https://elbayoumi.github.io/Ecommerce-OOP/chat.html");
 
 // socket.on("connect_error", (err) => {
@@ -19,39 +20,36 @@ app.get('/', (req, res) => {
 })
 io.on('connection', (socket) => {
     const userToken = socket.handshake.query.token;
+    verify(userToken, JWT_SECRET_KEY)
+        try {
 
-    try {
-        const decodedToken = jwt.verify(userToken, JWT_SECRET_KEY);
-    
-        // تحقق من بعض المعلومات الإضافية في التوكن إذا لزم الأمر
-        // مثال: const userId = decodedToken.userId;
-    
-        // console.log('User authenticated:', decodedToken);
-    
-      } catch (error) {
-        // console.error('Token verification failed:', error.message);
-      }
+            // console.log('User authenticated:', decodedToken);
+            socket.onAny((eventName, data) => {
+                // Emit an event with a dynamic response name in response to any received event
+                io.emit('r' + eventName, data);
+            });
 
-      socket.onAny((eventName, data) => {    
-        // Emit an event with a dynamic response name in response to any received event
-        io.emit('r'+eventName, data);
-      });
+            socket.on("customEvent", (eventName, messege) => {
+                // console.log(messege);
+                let messages = JSON.parse(messege)
+                io.sockets.emit(eventName, JSON.stringify(messages));
+
+            })
+            socket.on('desconnect', (socket) => {
+                console.log('Desconnect');
+            })
+        } catch (error) {
+            // console.error('Token verification failed:', error.message);
+        }
+
     
-    socket.on("customEvent", (eventName,messege) => {
-        // console.log(messege);
-        let token = JSON.parse(messege).token
-        let messages =JSON.parse(messege)
-        delete messages.token
-            if (checkToken(messege,token)) {
-            io.sockets.emit(eventName, JSON.stringify(messages));
-            }
-    })
-    socket.on('desconnect', (socket) => {
-        console.log('Desconnect');
-    })
+
+
 });
-function checkToken(messege,token) {
-    return token == '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' ? JSON.parse(messege).type : false
+const verify = (userToken, JWT_SECRET_KEY) => {
+    if(userToken != JWT_SECRET_KEY ){
+        throw new Error('not authorized');
+    }
 }
 server.listen(3000, () => {
     console.log("Server listening on port 3000 is runing")
